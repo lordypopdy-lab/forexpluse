3c472fe0992011bf126e5772d3db7a749d54bdae
lordypopdy-lab <deelordpopdy2@gmail.com>
2025-01-23 21:28:42 -0800

    safe

--- forexpluseserver/routes/authRoute.js
+++ forexpluseserver/routes/authRoute.js
@@ -3,13 +3,13 @@ const router = express.Router();
 const cors = require("cors");
 
 const corsOptions = {
-    origin: 'http://localhost:5173', 
+    origin: 'https://forexpluse.vercel.app', 
     credentials: true,
     methods: 'GET,POST,PUT,DELETE,OPTIONS',
     allowedHeaders: 'Content-Type,Authorization',
   };
 
-  //https://tradevister.vercel.app
+  //https://forexpluse.vercel.app
   
   router.use(cors(corsOptions));
   router.options('*', cors(corsOptions)); 
